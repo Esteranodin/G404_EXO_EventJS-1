a31ba4db2509e1ce535b5d54240f69a71c432343
esteranodin <esteranodin@hotmail.fr>
2024-11-18 15:49:26 +0100

    DONE : Exo 4

---
+++ Exercice_4/script.js
@@ -0,0 +1,6 @@
+const buttonRAZ = document.querySelector('button');
+
+buttonRAZ.addEventListener('click', function handleResetAll () {
+    let inputs = document.querySelectorAll('input');
+inputs.forEach((input => {input.value = ''}));
+});
