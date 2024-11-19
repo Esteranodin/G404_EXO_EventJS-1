605251b0656e6a82aa06d082591b99104a0358a5
esteranodin <esteranodin@hotmail.fr>
2024-11-19 10:08:16 +0100

    ADD : Exo 6

--- Exercice_6/script.js
+++ Exercice_6/script.js
@@ -7,7 +7,12 @@ function handleMouseEnter(event){
     
     const imgTag = event.target;
     imgTag.src = imgTag.src.replace('.jpg', '_2.jpg'); 
+};
 
-    imgTag.removeEventListener('mouseenter', handleMouseEnter);
+imageHover.forEach(image => {image.addEventListener ('mouseleave', handleMouseLeave);
+});
 
+function handleMouseLeave(event) {
+const imgTag = event.target;
+imgTag.src = imgTag.src.replace('_2.jpg', '.jpg');
 };
